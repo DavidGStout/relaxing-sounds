c21495596c9adf96b116ce7a0dea8fe1d2981d2f
DavidGStout <davidgstout63@gmail.com>
2018-01-27 19:26:19 -0500

    Added volume control

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ function stopMusic(){
 	player.pause();
 	player.currentTime=0;
 }
+
+function volDown() {
+
+	console.log(player.volume);
+	if(player.volume >=0) {
+		player.volume-=0.1;
+	}else{
+		player.volume=0;
+	}
+}
+
+function volUp() {
+
+	console.log(player.volume);
+	if(player.volume >=0) {
+		player.volume+=0.1;
+	}else{
+		player.volume=0;
+	}
+}
